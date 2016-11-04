903f4989427eeb852a1045d4360762259f4c0e21
zhanchunliang <zhanchunliang@ele.me>
2016-11-04 11:30:21 +0800

    start

--- src/main/java/com/zcl/somecode/design/Observer1.java
+++ src/main/java/com/zcl/somecode/design/Observer1.java
@@ -2,7 +2,6 @@ package com.zcl.somecode.design;
 
 public class Observer1 implements Observer {
 
-	@Override
 	public void update() {
 		// TODO Auto-generated method stub
 		System.out.println("observer1 has received");
